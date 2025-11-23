a1eaa55acc3fa8ddb7de39d9049f9260f553af53
Khushi Desai <khushipdesai@gmail.com>
2025-11-23 08:17:20 +0000

    fix import issues in init

--- src/sea/data/__init__.py
+++ src/sea/data/__init__.py
@@ -1,5 +1,5 @@
-import data.samplers
-from data.utils import run_gies, run_fci, collate, convert_to_graphs
-from data.data import DataModule, InferenceDataModule, BaselineDataModule
-from data.dataset import MetaObservationalDataset
+from .samplers import *
+from .utils import run_gies, run_fci, collate, convert_to_graphs
+from .data import DataModule, InferenceDataModule, BaselineDataModule
+from .dataset import MetaObservationalDataset
 
