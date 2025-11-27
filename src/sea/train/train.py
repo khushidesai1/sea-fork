ae33f10e8f045572d302daa43f7f89575d5c6302
Khushi Desai <khushipdesai@gmail.com>
2025-11-27 05:54:14 +0000

    update to use wandb on flag

--- src/sea/train/train.py
+++ src/sea/train/train.py
@@ -124,7 +124,7 @@ def train_sea(
     printt("Finished loading model.")
 
     # Logger
-    if args.debug:
+    if args.use_wandb:
         wandb_logger = None
     else:
         run_id = str(time.time())
